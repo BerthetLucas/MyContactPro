9281d19c0a1db0f4bcc673e82e2331d62f5fcc39
Lucas Berthet <lucasberthet@MacBook-Pro-de-Lucas-2.local>
2025-06-06 16:54:10 +0200

    add update operation

--- app/src/main/java/com/example/mycontactpro/DetailActivity.java
+++ app/src/main/java/com/example/mycontactpro/DetailActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,7 +25,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private Context context;
     private TextView tvdFirstName, tvdLastName, tvdCompany, tvdTel, tvdMail, tvdSector, tvdAddress;
-    private Button callButton, locationButton, smsButton;
+    private Button callButton, locationButton, smsButton, favButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +73,7 @@ public class DetailActivity extends AppCompatActivity {
         tvdMail = findViewById(R.id.tvdEmail);
         tvdSector = findViewById(R.id.tvdSector);
         tvdAddress = findViewById(R.id.tvdAddress);
+        favButton = findViewById(R.id.favButton);
 
         callButton = findViewById(R.id.callButton);
         locationButton = findViewById(R.id.locationButton);
@@ -86,6 +89,12 @@ public class DetailActivity extends AppCompatActivity {
         tvdMail.setText(contact.getEmail());
         tvdSector.setText(contact.getSecteur());
         tvdAddress.setText(contact.getAddresse());
+
+        if (contact.getFavori() == 0) {
+            favButton.setText("Ajouter aux favoris");
+        } else {
+            favButton.setText("Retirer des favoris");
+        }
     }
 
     private void setupButtons(Contact contact) {
@@ -107,11 +116,45 @@ public class DetailActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        favButton.setOnClickListener(v -> {
+            if (contact.getFavori() == 0) {
+                contact.setFavori(1);
+                ContactDatabase.getDb(context).contactDAO().update(contact);
+                loadContactData(contact);
+            } else {
+                contact.setFavori(0);
+                ContactDatabase.getDb(context).contactDAO().update(contact);
+                loadContactData(contact);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        long contactId = getIntent().getLongExtra("contact_id", -1L);
+        Contact contact = ContactDatabase.getDb(context).contactDAO().getById(contactId);
+
+        if (item.getItemId() == R.id.updateContact) {
+            Intent intent = new Intent(context, UpdateContactActivity.class);
+            intent.putExtra("contact_id", contact.getId());
+            startActivity(intent);
+            return true;
+        }
+
+        if (item.getItemId() == R.id.deleteContact) {
+
+            ContactDatabase.getDb(context).contactDAO().delete(contact);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/mycontactpro/UpdateContactActivity.java
+++ app/src/main/java/com/example/mycontactpro/UpdateContactActivity.java
@@ -1,14 +1,31 @@
 package com.example.mycontactpro;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mycontactpro.pojos.Contact;
+import com.example.mycontactpro.pojos.ContactDatabase;
+
 public class UpdateContactActivity extends AppCompatActivity {
+
+    private EditText firstName, lastName, company, address, tel, mail;
+    private Spinner sectorSpinner;
+    private Button updateButton;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,107 @@ public class UpdateContactActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        context = getApplicationContext();
+        long contactId = getIntent().getLongExtra("contact_id", -1L);
+        Contact contact = ContactDatabase.getDb(context).contactDAO().getById(contactId);
+        initInterface();
+        initSpinner();
+        loadContactInformation(contact);
+        initButton(contact);
     }
+
+    private void initInterface() {
+
+        Toolbar toolbar = findViewById(R.id.toolbarUpdateContact);
+        setSupportActionBar(toolbar);
+        firstName = findViewById(R.id.tv_update_firstName);
+        lastName = findViewById(R.id.tv_update_lastName);
+        company = findViewById(R.id.tv_update_company);
+        address = findViewById(R.id.tv_update_address);
+        tel = findViewById(R.id.tv_update_tel);
+        mail = findViewById(R.id.tv_update_mail);
+        updateButton = findViewById(R.id.updateButton);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void initButton(Contact contact) {
+        updateButton.setOnClickListener(v -> updateContact(contact));
+    }
+
+    private void initSpinner() {
+        sectorSpinner = findViewById(R.id.tv_update_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.sector,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sectorSpinner.setAdapter(adapter);
+    }
+
+    private void loadContactInformation(Contact contact) {
+        firstName.setText(contact.getPrenom());
+        lastName.setText(contact.getNom());
+        address.setText(contact.getAddresse());
+        tel.setText(contact.getTel());
+        mail.setText(contact.getEmail());
+        company.setText(contact.getSociete());
+    }
+
+
+    public void updateContact(Contact contact) {
+        if (areFieldFill()) {
+            Contact updatedcontact = new Contact(
+                    lastName.getText().toString(),
+                    firstName.getText().toString(),
+                    company.getText().toString(),
+                    address.getText().toString(),
+                    tel.getText().toString(),
+                    mail.getText().toString(),
+                    sectorSpinner.getSelectedItem().toString(),
+                    0
+            );
+
+            contact.setAddresse(updatedcontact.getAddresse());
+            contact.setNom(updatedcontact.getNom());
+            contact.setPrenom(updatedcontact.getPrenom());
+            contact.setEmail(updatedcontact.getEmail());
+            contact.setSociete(updatedcontact.getSociete());
+            contact.setSecteur(updatedcontact.getSecteur());
+            contact.setTel(updatedcontact.getTel());
+
+            ContactDatabase.getDb(context).contactDAO().update(contact);
+            Toast.makeText(UpdateContactActivity.this,
+                    "Contact mis à jour", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(UpdateContactActivity.this,
+                    "Veuillez remplir tout les champs", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private boolean areFieldFill() {
+        return !firstName.getText().toString().trim().isEmpty() &&
+                !lastName.getText().toString().trim().isEmpty() &&
+                !company.getText().toString().trim().isEmpty() &&
+                !address.getText().toString().trim().isEmpty() &&
+                !tel.getText().toString().trim().isEmpty() &&
+                !mail.getText().toString().trim().isEmpty();
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
 }
